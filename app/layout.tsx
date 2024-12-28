1430de6d584398022177ee723b3e0f07ebdf2330
monkaCode <f.h612@hotmail.com>
2024-12-28 23:20:16 +0100

    modified toolbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({children}: { children: React.ReactNode }) {
         <html lang="en">
         <body>
         <Providers themeProps={{attribute: "class", defaultTheme: "dark"}}>
-            <Navbar title={"Images Gallery"}/>
+            <Navbar title={"Valolytics Images"} icon={"https://imagedelivery.net/WUSOKAY-iA_QQPngCXgUJg/e30fb45b-1f85-4cae-5b52-9c733d0c8f00/w=40"}/>
             <main className="container mx-auto my-auto max-w-7xl pt-16 px-6 flex-grow mb-5">
                 {children}
             </main>
--- components/globalnav/navbar.tsx
+++ components/globalnav/navbar.tsx
@@ -9,23 +9,38 @@ import NextLink from "next/link";
 import { ThemeSwitch } from "./theme-switch";
 import {CloudIcon} from "@/components/globalnav/icons";
 
-export const Navbar = ({title, icon, link}: {title: string, icon?: React.ReactNode, link?: string}) => {
+import Image from 'next/image';
 
-	return (
-		<NextUINavbar maxWidth="xl" position="sticky">
-			<NavbarContent className="basis-1/5 sm:basis-full" justify="start">
-				<NavbarBrand as="li" className="gap-3 max-w-fit">
-					<NextLink className="flex justify-start items-center gap-1" href={link ?? '/'}>
-						{
-							icon ? icon : <CloudIcon color={'orange'} />
-						}
-						<p className="font-bold text-inherit">{title}</p>
-					</NextLink>
-				</NavbarBrand>
-			</NavbarContent>
-			<NavbarContent className="basis-1/5 sm:basis-full" justify="end">
-				<ThemeSwitch />
-			</NavbarContent>
-		</NextUINavbar>
-	);
+export const Navbar = ({
+  title,
+  icon,
+  link,
+}: {
+  title: string;
+  icon?: React.ReactNode | string; // Allow ReactNode or string (image source)
+  link?: string;
+}) => {
+  return (
+    <NextUINavbar maxWidth="xl" position="sticky">
+      <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
+        <NavbarBrand as="li" className="gap-3 max-w-fit">
+          <NextLink className="flex justify-start items-center gap-1" href={link ?? '/'}>
+            {icon ? (
+              typeof icon === 'string' ? (
+                <Image src={icon} alt={`${title} logo`} width={30} height={30} />
+              ) : (
+                icon
+              )
+            ) : (
+              <CloudIcon color={'orange'} />
+            )}
+            <p className="font-bold text-inherit">{title}</p>
+          </NextLink>
+        </NavbarBrand>
+      </NavbarContent>
+      <NavbarContent className="basis-1/5 sm:basis-full" justify="end">
+        <ThemeSwitch />
+      </NavbarContent>
+    </NextUINavbar>
+  );
 };
